8543f9fc76e046dff48a88a44ee6740febc9746c
Nat Waterworth <32358976+ProgrammerWaterworth@users.noreply.github.com>
2021-02-21 00:03:05 +0000

    Reorganised Files

---
+++ Unity3D/Assets/Genetic Algorithm/Scripts/ChromosomeData.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[Serializable]
+[CreateAssetMenu(fileName = "chromosome", menuName = "Genetic Algorithm/Chromosome")]
+public class ChromosomeData : ScriptableObject
+{
+    [SerializeField] GameObject gameObject;
+    [SerializeField] List<GeneData> genes;
+    public void GetCurrentGameObjectGenes()
+    {
+        if (gameObject == null)
+            return;
+
+        genes.Clear();
+        Component[] cs = (Component[])gameObject.GetComponents(typeof(Component));
+        foreach (Component c in cs)
+        {
+            foreach (FieldInfo fi in c.GetType().GetFields())
+            {
+                if (fi.GetValue(c).GetType().Equals(typeof(System.Single)))
+                {
+                    GeneData gd = new GeneData(c.GetType().Name + " - "+fi.Name, (float)fi.GetValue(c));
+                    genes.Add(gd);
+                    Debug.Log("field name " + fi.Name + " val " + fi.GetValue(c));
+                }
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Update Genetic Algorithm with set of genes to iterate on.
+    /// </summary>
+    public void UpdateGeneticAlgorithm()
+    {
+        GeneticAlgorithm geneticAlgorithm = GameObject.FindObjectOfType<GeneticAlgorithm>();
+
+        if (geneticAlgorithm != null)
+        {
+            //Get the genes of the current individual and apply them to the genetic algorithm.
+            float[] _genes = new float[genes.Count];
+            for(int i = 0; i < genes.Count; i++)
+            {
+                _genes[i] = genes[i].GetValue();
+            }
+            Debug.Log("Setting Genetic Algorithm to " + gameObject.name);
+            geneticAlgorithm.SetGenes(_genes);
+        }
+        else
+        {
+            Debug.LogError("No existing Genetic Algorithm. Create one.");
+        }
+    }
+
+    /// <summary>
+    /// Updates the genes of the gameobject. Will not function properly if the ordering of components is altered or genes have been removed/added.
+    /// </summary>
+    public void ApplyGenesToGameObject()
+    {
+        if (gameObject == null)
+            return;
+
+        int index = 0;
+
+        Component[] cs = (Component[])gameObject.GetComponents(typeof(Component));
+        foreach (Component c in cs)
+        {
+            foreach (FieldInfo fi in c.GetType().GetFields())
+            {
+                if (fi.GetValue(c).GetType().Equals(typeof(System.Single)))
+                {
+                    //if the retrieved value == the index of current one update its value.
+                    if (genes[index].GetName().Equals(c.GetType().Name + " - " + fi.Name))
+                    {
+                        Debug.Log("Found gene, altering: " + genes[index].GetName() +" from "+ fi.GetValue(c) + " to " + genes[index].GetValue());
+                        fi.SetValue(c,genes[index].GetValue());
+
+                        //inform Unity that the insance has been modified
+                        EditorUtility.SetDirty(c);
+                        index++;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Unity3D/Assets/Genetic Algorithm/Scripts/GeneticAlgorithm.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneticAlgorithm : MonoBehaviour
+{
+    public static GeneticAlgorithm Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    [SerializeField] TestAgent agent;
+    /// <summary>
+    /// The weights of the genes that dictate the agents behaviour.
+    /// </summary>
+    [SerializeField] float[] genes;
+
+    /// <summary>
+    /// Port to Python Implementation of Genetic Algorithm. Feed it a fitness and it will return chromosome weights.
+    /// </summary>
+    InformationPort port;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<InformationPort>() != null)
+        {
+            port = GetComponent<InformationPort>();
+        }
+        else Debug.LogError(this + " requires an information port component");
+
+        StartCoroutine(DelayedFitnessReport());
+    }
+
+    public void SetGenes(float[] _genes)
+    {
+        genes = _genes;
+    }
+
+    /// <summary>
+    /// Calculate fitness from agents ability to survive.
+    /// </summary>
+    float CalculateFitness()
+    {
+        float _fitness = 0;
+
+        if (agent != null) //Debug#2 - Testing on agent
+        {
+            agent.SetRangeGene(genes[0]);
+            int _count = agent.CheckForBatteries();
+            _fitness = _count +1;
+        }
+        else //Debug #1 - Testing if fitness affects ga
+        {
+            for (int i = 0; i < genes.Length; i++)
+            {
+                _fitness = genes[i];
+            }
+            _fitness /= genes.Length;
+        }
+        return _fitness;
+    }
+
+    IEnumerator DelayedFitnessReport()
+    {
+        if (port != null)
+        {
+            port.SetDataOut(genes);
+        }
+
+
+        while (true)
+        {
+            yield return new WaitForSeconds(.1f);
+            SendFitness();
+        }
+    }
+
+    /// <summary>
+    /// Sends fitness 
+    /// </summary>
+    void SendFitness()
+    {
+        Debug.Log(port);
+        if (!port.GetArray().Equals(genes))
+        {
+            genes = port.GetArray();
+            float[] _fitness = new float[1];
+            _fitness[0] = CalculateFitness();
+            port.SetDataOut(_fitness);
+        }
+        else
+            Debug.Log("genes are still the same...");
+    }
+}
